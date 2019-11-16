7b9c5c37cc0770ab1e9ebd5f8b849a1c182cdeff
Mario <mariomicetic@gmail.com>
2019-11-16 12:03:12 +0100

    Updates

---
+++ precache-manifest.0feee3c6a1e0440739edc0ac15391cf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99b9c47446db19f3727c4f88d150af7",
+    "url": "/roomcontroller/index.html"
+  },
+  {
+    "revision": "741ffce9021b68153650",
+    "url": "/roomcontroller/static/css/main.5a456527.chunk.css"
+  },
+  {
+    "revision": "a8e8cd79349bc74bd5e4",
+    "url": "/roomcontroller/static/js/2.7140194f.chunk.js"
+  },
+  {
+    "revision": "741ffce9021b68153650",
+    "url": "/roomcontroller/static/js/main.8da2daab.chunk.js"
+  },
+  {
+    "revision": "c57ce13767c234a62ef9",
+    "url": "/roomcontroller/static/js/runtime-main.f22fe4f6.js"
+  }
+]);
